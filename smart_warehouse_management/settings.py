94ce87a96bc244f1d39985802d87a9bb79e30e35
Sanjana Poptani <202012044@daiict.ac.in>
2025-02-28 13:51:24 +0530

    changed admin and added poetry

--- smart_warehouse_management/settings.py
+++ smart_warehouse_management/settings.py
@@ -118,6 +118,50 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+JAZZMIN_SETTINGS = {
+    "site_title": "My Project Admin",
+    "site_header": "My Project",
+    "welcome_sign": "Welcome to the admin panel",
+    "site_brand": "My Brand",
+    "copyright": "My Company",
+    "search_model": [
+        "users.CustomUser",
+        "inventory.Fleet",
+    ],  # Allows searching in Students model
+    "show_ui_builder": True,  # Show UI builder for customizing appearance
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": False,
+    "accent": "accent-primary",
+    "navbar": "navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-dark-primary",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "slate",
+    "dark_mode_theme": None,
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
